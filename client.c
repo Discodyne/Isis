eb2cf34761192c41af2117b401bf0b442ba57fc8
Discodyne <rivs.70@mail.ru>
2015-11-27 10:45:30 +0300

    ftp.ver.final

--- client.c
+++ client.c
@@ -12,18 +12,6 @@ const int ans_size = 5000;
 
 int main(void) //int argc, char*argv[])
 {
-    //char* command = (char*)malloc(sizeof(char)*5);
-    //char* path = (char*)malloc(sizeof(char)*buf_size);
-    /*if (argc < 3)
-    {
-        printf("Недостаточное число аргументов\n");
-        return 0;
-    }
-    else
-    {
-        command = argv[1];
-        path = argv[2];
-    }*/
     char* command = (char*)malloc(sizeof(char)*buf_size);
 
     int sockfd;
@@ -34,12 +22,12 @@ int main(void) //int argc, char*argv[])
     //buf[0] = command;
     //buf[1] = path;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);//Инициализируем клиентский сокет
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = inet_addr("127.0.0.1");
     address.sin_port = 9734;
     len = sizeof(address);
-    result = connect(sockfd, (struct sockaddr*)&address, len);
+    result = connect(sockfd, (struct sockaddr*)&address, len);//Соединение с сервером
     if (result == -1)
     {
         perror("client error");
@@ -55,7 +43,7 @@ int main(void) //int argc, char*argv[])
         if (strcasecmp(command,"q\n"))
         {
             char* answer = (char*)malloc(sizeof(char)*ans_size);
-            read(sockfd, answer, ans_size);
+            read(sockfd, answer, ans_size);//Считываем ответ с сервера
             printf("%s",answer);
             answer = "\n";
         }
